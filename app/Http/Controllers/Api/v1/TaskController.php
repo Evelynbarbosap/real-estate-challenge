6c40cb9f9e3ab63a9f86e592a6ceb26074f27c04
Evelyn Pombal Barbosa <evelyn.barbosa@ish.com.br>
2024-06-20 20:52:11 -0300

    Creating a route to list the tasks of a building

---
+++ app/Http/Controllers/Api/v1/TaskController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use Carbon\Carbon;
+use App\Models\Building;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+
+class TaskController extends Controller
+{
+    public function index(Building $building)
+    {
+        $tasks = $building->tasks()
+        ->with(['user', 'comments.user'])
+        ->get()
+        ->map(function ($task) {
+            return [
+                'id' => $task->id,
+                'title' => $task->title,
+                'description' => $task->description,
+                'assigned_to' => $task->user->name,
+                'status' => $task->status,
+                'due_date' => $task->due_date ? Carbon::parse($task->due_date)->format('d/m/Y H:i') : null,
+                'comments' => $task->comments->map(function ($comment) {
+                    return [
+                        'id' => $comment->id,
+                        'comment' => $comment->comment,
+                        'user_id' => $comment->user->id,
+                        'user_name' => $comment->user->name,
+                        'created_at' => $comment->created_at ? Carbon::parse($comment->created_at)->format('d/m/Y H:i') : null
+                    ];
+                }),
+                'created_at' => $task->created_at  ? Carbon::parse($task->created_at)->format('d/m/Y H:i') : null,
+                'updated_at' => $task->updated_at  ? Carbon::parse($task->created_at)->format('d/m/Y H:i') : null,
+            ];
+        });
+
+        return response()->json($tasks);
+    }
+
+}
