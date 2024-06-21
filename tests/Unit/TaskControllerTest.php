503f702b92d8ec0da31324cfb5d761e1315ff435
Evelyn Pombal Barbosa <evelyn.barbosa@ish.com.br>
2024-06-21 16:39:40 -0300

    Add unit test for title, status, assigned to

--- tests/Unit/TaskControllerTest.php
+++ tests/Unit/TaskControllerTest.php
@@ -42,4 +42,70 @@ class TaskControllerTest extends TestCase
             'building_id' => $building->id,
         ]);
     }
+
+    public function test_store_method_task_requires_title()
+    {
+        $building = Building::factory()->create();
+        $user = User::factory()->create();
+
+        $status = ['open', 'progress', 'completed', 'rejected'];
+        $randomStatus = $status[array_rand($status)];
+        
+        $currentDate = Carbon::now()->format('Y-m-d');
+
+        $taskData = [
+            'title' => Null,
+            'description' => 'This is a test task',
+            'assigned_to' => $user->id,
+            'status' => $randomStatus,
+            "due_date" => $currentDate
+        ];
+
+        $response = $this->json('POST', "/api/v1/tasks/building/{$building->id}", $taskData);
+      
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors('title');
+    }
+    
+    public function test_store_method_task_requires_status()
+    {
+        $building = Building::factory()->create();
+        $user = User::factory()->create();
+
+        $currentDate = Carbon::now()->format('Y-m-d');
+
+        $taskData = [
+            'title' => 'Unit Test Task',
+            'description' => 'This is a test task',
+            'assigned_to' => $user->id,
+            "due_date" => $currentDate
+        ];
+
+        $response = $this->json('POST', "/api/v1/tasks/building/{$building->id}", $taskData);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors('status');
+    }
+    
+    public function test_store_method_task_requires_assigned_to()
+    {
+        $building = Building::factory()->create();
+        
+        $status = ['open', 'progress', 'completed', 'rejected'];
+        $randomStatus = $status[array_rand($status)];
+        
+        $currentDate = Carbon::now()->format('Y-m-d');
+
+        $taskData = [
+            'title' => 'Unit Test Task',
+            'status' => $randomStatus,
+            'description' => 'This is a test task',
+            "due_date" => $currentDate
+        ];
+
+        $response = $this->json('POST', "/api/v1/tasks/building/{$building->id}", $taskData);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors('assigned_to');
+    }
 }
